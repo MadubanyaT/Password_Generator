b3029254b499e283ad66a4d5a1078e53700ce671
Tshepho M <madubanyatshepo85@gmail.com>
2025-03-25 18:52:00 +0200

    update the Encoding file

--- Encoding.py
+++ Encoding.py
@@ -1,6 +1,10 @@
 import struct
 
-from cryptography.fernet import Fernet
+import cryptography
+import os
+
+from cryptography.exceptions import InvalidSignature
+from cryptography.fernet import Fernet, InvalidToken
 
 
 # This class should not output anything, but write to files.
@@ -8,31 +12,62 @@ from cryptography.fernet import Fernet
 # Must have to encrypt, decrypt, createKey methods
 # It can update the user if the key is created
 
-class Encoding:
 
-    def __init__(self):
-        self.__websiteName__ = "Unknown Website"
-        self.__userName__ = "Unknown Username"
-        self.__secretKey__ = ""
+class Encoding:
+    def __init__(self, Key=None):
+        self.__secretKey__ = Key or Fernet.generate_key()
+        self.__cipher__ = Fernet(self.__secretKey__)
         self.__info__ = ""
 
-    def Encrypt(self, _webName, _userName, _password, _filename):
-        key = __generateKey__(self)
+    def Encrypt(self, _filename, _password, _webName="Unknown Website", _userName="Unknown Username", ):
+        self.__saveKey__()
+        # fernet = Fernet(self.__secretKey__)
 
-        fernet = Fernet(key)
+        try:
+            # file_w = open(_filename, 'a')
+            text = ('Website name: ' + _webName + ', Username: '
+                    + _userName + ', Password: ' + _password + '\n').encode('utf-8')
 
-        file = open(_filename, '')
+            encrypted = self.__cipher__.encrypt(text)
+
+            print(encrypted)
+            file_w = open(_filename, 'wb')
+            file_w.write(encrypted)
+        except:
+            raise Exception('An error occurred.')
+
+        file_w.close()
+        print(self.__secretKey__)
+
+    def Decrypt(self, _encryptedFilename, _decryptedFilename):
+        try:
+            file_r = open(_encryptedFilename, 'rb')
+            encrypted = file_r.read()
+
+            decrypted = self.__cipher__.decrypt(encrypted)
+
+            file_w = open(_decryptedFilename, 'wb')
+            file_w.write(decrypted)
+        except cryptography.exceptions as c:
+            return 'Failed'
+
+        file_w.close()
+        file_r.close()
+        print('Decryption was a success')
+
+    def __saveKey__(self):
+        try:
+            filekey = open('filekey.txt', 'wb')
+            filekey.write(self.__secretKey__)
+        except:
+            raise Exception('Failed to write to the generated key.')
+
+        filekey.close()
+        self.__info__ += "The key has been generated and saved in the 'filekey.key' file."
 
 
-def __generateKey__(self):
-    key = Fernet.generate_key()
+E1 = Encoding()
 
-    try:
-        filekey = open('filekey.key', 'wb')
-        filekey.write(key)
-    except:
-        raise Exception('Failed to write to the generated key.')
+#E1.Encrypt('Test1', '12345')
 
-    filekey.close()
-    self.__info__ += "The key has been generated and saved in the 'filekey.key' file."
-    return key
+E1.Decrypt('Test1', 'newTest1')
